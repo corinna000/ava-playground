ee24bc805885b849c26b1ed1f6dd51feb1381e86
Corinna Cohn <corinna000@gmail.com>
2016-02-08 14:51:51 -0500

    Initial

---
+++ test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import delay from 'delay';
+
+test('foo', t => {
+    t.pass();
+});
+
+test('bar', async t => {
+    t.plan(1);
+
+    const bar = Promise.resolve('bar').then(delay(200));
+
+    t.is(await bar, 'bar');
+});
